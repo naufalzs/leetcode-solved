f54277eabf8d67766e1d079e17672061ff3990cd
zufar <32706126+naufalzs@users.noreply.github.com>
2025-04-10 06:38:57 +0700

    Time: 0 ms (100%), Space: 54 MB (35.21%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -3,12 +3,30 @@
  * @param {number} target
  * @return {number}
  */
+// loop
+// var searchInsert = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === target || nums[i] > target) {
+//       return i;
+//     }
+//   }
+
+//   return nums.length;
+// };
+
+// Binary Search
 var searchInsert = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target || nums[i] > target) {
-      return i;
+  let l = 0;
+  let r = nums.length - 1;
+  while (l <= r) {
+    mid = Math.floor((l + r) / 2);
+    if (nums[mid] < target) {
+      l = mid + 1;
+    } else if (nums[mid] > target) {
+      r = mid - 1;
+    } else {
+      return mid;
     }
   }
-
-  return nums.length;
+  return l;
 };
